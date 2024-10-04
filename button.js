9ecbb3c6604cbfe688c31829f99086e4fde270b6
Kishore V <mugikish2005@gmail.com>
2024-10-04 14:52:39 +0530

    button.js

--- button.js
+++ button.js
@@ -1,21 +1,21 @@
 function changeRed(){
     const red=document.getElementById('red');
-    red.style.backgroundColor="red";
+    document.body.style.backgroundColor = "red";
 }
 function changeBlue(){
     const blue=document.getElementById('blue');
-    blue.style.backgroundColor="blue";
+    document.body.style.backgroundColor = "blue";
 }
 function changeGreen(){
     const green=document.getElementById('green');
-    green.style.backgroundColor="green";
+    document.body.style.backgroundColor = "green";
 }
 function reset(){
     const red=document.getElementById('red');
-    red.style.backgroundColor="";
+    document.body.style.backgroundColor = "";
     const blue=document.getElementById('blue');
-    blue.style.backgroundColor="";
+    document.body.style.backgroundColor = "";
     const green=document.getElementById('green');
-    green.style.backgroundColor="";
+    document.body.style.backgroundColor = "";
 
 }
